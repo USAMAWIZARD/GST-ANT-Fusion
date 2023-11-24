0cc7464f2c9bf889238c27f460e6aa7e0a6acfe1
USAMAWIZARD <usamawizard@gmail.com>
2023-11-25 01:37:04 +0530

    open src now

--- test/curl_api.sh
+++ test/curl_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
-#ffmpeg -stream_loop -1 -re -i ./high.mp4 -c:v libx264 -an -f mpegts "srt://localhost:4200?streamid=LiveApp/stream111" &
-ffmpeg -stream_loop 1 -re -i ./high.mp4 -c:v libx264 -c:a aac -f flv "rtmp://localhost/LiveApp/stream111" &
+#ffmpeg -stream_loop -1 -re -i ./test.mp4 -c:v libx264 -an -f mpegts "srt://localhost:4200?streamid=LiveApp/stream111" &
+ffmpeg -stream_loop 1 -re -i ./test.mp4 -c:v libx264 -c:a aac -f flv "rtmp://localhost/LiveApp/stream111" &
 sleep 5
 if [[  $1 == "RTSP" ]]; then
 curl -X POST -H "Content-Type: application/json"  -d '{"streamId":"stream111","pipeline_type":"RTSP_OUT","protocol":"TCP"}' http://localhost:5080/LiveApp/rest/pipeline/register-pipeline/
--- test/loadtest.sh
+++ test/loadtest.sh
@@ -1,4 +1,13 @@
+#Publish
 for i in {0..50};
+do
+	ffmpeg -stream_loop 12 -re -i ./test.mp4 -c:v copy -c:a aac -f flv "rtmp://localhost/LiveApp/myStream-$i" &
+done
+
+read -p "Press enter to Play"
+
+#Play
+for i in {0..20};
 do
 	ffplay  rtsp://127.0.0.1:8554/myStream-$i &
 done
